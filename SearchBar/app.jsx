40342de5f3e7aeac0dc9e7ad754bcd818541da0b
Aksh4code <akkisingh8266@gmail.com>
2024-09-28 02:46:24 +0530

    Search bar using react

---
+++ SearchBar/app.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+
+const data = [
+{
+  name : "hritik",
+  mail : "hritik@hritik.com"
+},
+{
+  name : "mridul",
+  mail : "mridul@mridul.com"
+  
+}
+]
+
+export default function App(){
+
+  const [input,setInput] = useState('')
+  const [result,setResult] = useState([])
+
+  const handleClick = (event) => {
+
+    const query = event.target.value.toLowerCase();
+    setInput(query)
+
+    const results = data.filter(item => item.name.toLowerCase().includes(query))
+
+    setResult(results)
+    console.log(results); 
+  }
+
+  return (
+    <>
+      <input type="text" value={input} onChange={handleClick}/>
+      <div>
+        {
+          result.map((item, index) => {
+            return (
+            <div key={index}>
+              <h1>{item.name}</h1>
+              <p>{item.mail}</p>
+            </div>
+            )
+          })
+        }
+      </div>
+    </>
+  )
+
+}
